22b07b9017942286212e7c02ea0176ccf289cbce
Mira Hedl <miroslav.hedl@merck.com>
2018-12-09 11:20:33 +0100

    Simplest java project - print hello world. Initial commit.

---
+++ src/main/java/org/kurz/App.java
@@ -0,0 +1,13 @@
+package org.kurz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Jenkins!" );
+    }
+}
